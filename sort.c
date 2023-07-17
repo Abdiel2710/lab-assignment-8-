4a3eb81bd95e4857f3c5250a7ae6877df5476794
Abdiel2710 <137232488+Abdiel2710@users.noreply.github.com>
2023-07-16 22:25:50 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -8,6 +8,22 @@ int extraMemoryAllocated;
 // extraMemoryAllocated counts bytes of memory allocated
 void heapSort(int arr[], int n)
 {
+	 int i, j;
+    for (i = 0; i < n-1; i++)
+    {
+        int swaps = 0;
+        for (j = 0; j < n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap(&arr[j], &arr[j+1]);
+                swaps++;
+            }
+        }
+
+        printf("pass #%d: %d\n", i + 1, swaps);
+
+    }
 }
 
 
@@ -15,6 +31,7 @@ void heapSort(int arr[], int n)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	
 }
 
 // parses input file to an integer array
